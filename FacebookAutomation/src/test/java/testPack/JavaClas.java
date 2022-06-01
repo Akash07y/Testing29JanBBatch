fcdbce2d107faeafd7b3b02eb969dd1153da3fcb
Akash <Akash@DESKTOP-B2220QC>
2022-06-01 21:05:34 +0530

    Edited test class user - 164646

--- FacebookAutomation/src/test/java/testPack/JavaClas.java
+++ FacebookAutomation/src/test/java/testPack/JavaClas.java
@@ -8,33 +8,6 @@ public final class JavaClas {
 
 	public static void main(String[] args){
 		
-		JavaClas g = new JavaClas();
-		g.finalize();
-		
-		int a = 10;
-		int b = 3;
-		int c = 56;
-		System.out.println("Hi");
-		int i = 9;
-		int x[] = {56,43,67,87};
-		
-		try {
-			c = a/b ;
-			System.out.println(x[i]);
-		}
-		finally {
-			System.out.println("finally block");
-		}
-		System.out.println(c);
-		System.out.println("END");
-		
-		//Exception not generated
-		// try -> finally -> normal flow
-		
-		// Exception generated and get handled 
-		// try -> catch -> finally -> normal flow
-		
-		// Exception id generated and not handle
-		// try -> finally
+		System.out.println("Huiiiiii");
 	}
 }
